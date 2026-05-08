d575b2edaf4790666df34d46d2b38665789eef98
mohammed-khalander <khalandermohammed734@gmail.com>
2026-05-09 02:30:42 +0530

    prisma migrations

--- apps/waves/prisma/migrations/20260429084246_waves/migration.sql
+++ apps/waves/prisma/migrations/20260429084246_waves/migration.sql
@@ -5,11 +5,23 @@ CREATE TYPE "MessageRole" AS ENUM ('USER', 'ASSISTANT');
 CREATE TYPE "MessageType" AS ENUM ('RESULT', 'ERROR');
 
 -- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Message" (
     "id" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "role" "MessageRole" NOT NULL,
     "type" "MessageType" NOT NULL,
+    "projectId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -29,8 +41,20 @@ CREATE TABLE "Fragment" (
     CONSTRAINT "Fragment_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Usage" (
+    "key" TEXT NOT NULL,
+    "points" INTEGER NOT NULL,
+    "expire" TIMESTAMP(3),
+
+    CONSTRAINT "Usage_pkey" PRIMARY KEY ("key")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Fragment_messageId_key" ON "Fragment"("messageId");
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Fragment" ADD CONSTRAINT "Fragment_messageId_fkey" FOREIGN KEY ("messageId") REFERENCES "Message"("id") ON DELETE CASCADE ON UPDATE CASCADE;
